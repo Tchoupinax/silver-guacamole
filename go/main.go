3cbcbbc36a87defef26057e13a9c4373131941f6
Tchoupinax <corentinfiloche@hotmail.fr>
2022-01-09 12:06:27 +0100

    test failing

--- go/main.go
+++ go/main.go
@@ -72,9 +72,9 @@ func postLink(w http.ResponseWriter, r *http.Request) {
 	}
 
 	data := map[string]interface{}{
-		"status": "success",
-		"code":   200,
-		"data":   linkBody,
+		"statusd": "success",
+		"code":    200,
+		"data":    linkBody,
 	}
 
 	payload, err := json.Marshal(data)
